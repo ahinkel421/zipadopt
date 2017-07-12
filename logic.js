c722014e74cffa9a4a58b0a5a4681f1afc588b74
Adam Hinkel <ahinkel421@gmail.com>
2017-07-12 11:14:56 -0400

    initial commit

--- logic.js
+++ logic.js
@@ -3,13 +3,18 @@ $(document).ready(function() {
 		$('#page-one').addClass('hidden');
 		$('#page-two').removeClass('hidden');
 		event.preventDefault();
-		getDataFromApi();
+		let animalChoice = $('.js-animal-choice').val();
+		let breedChoice = $('.js-breed-choice').val();
+		console.log(animalChoice);
+		getDataFromApi(animalChoice);
+		console.log(breedChoice);
+
 	});
+
 	$('#nav-header').click(function(event) {
-		$('#page-two').addClass('hidden');
-		$('#page-one').removeClass('hidden');
-	})
-	function getDataFromApi() {
+		location.reload();
+	});
+	function getDataFromApi(animalChoice) {
 		$.ajax({
 			url: "https://api.petfinder.com/pet.find",
 			type: 'Get',
@@ -17,71 +22,72 @@ $(document).ready(function() {
 				format: 'json',
 				key: 'fc1327e0ada9ec0e14f4de6009f2b8fa',
 				location: '20855',
-				breed: 'golden retriever'
+				animal: animalChoice
 			},
 			dataType: 'jsonp',
 			success: function(data) {
 
 				const petArray = data.petfinder.pets.pet;
 
-				console.log(petArray.length)
 				for(var i=0; i < petArray.length; i++) {
 					var currentPet = petArray[i];
+					if (currentPet.media.photos) {
 
-					const currentPetPic = currentPet.media.photos.photo[0].$t
-					const currentPetName = currentPet.name.$t;
-					const currentPetBreed = currentPet.breeds.breed[0]? currentPet.breeds.breed[0].$t : "unknown"
-					const currentPetAge = currentPet.age.$t;
-					const currentPetGender = currentPet.sex.$t;
-					const currentPetSize = currentPet.size.$t;
+						const currentPetPic = currentPet.media.photos.photo[0].$t;
+						const currentPetName = currentPet.name.$t;
+						const currentPetBreed = currentPet.breeds.breed[0]? currentPet.breeds.breed[0].$t : "Breed not listed";
+						const currentPetAge = currentPet.age.$t;
+						const currentPetGender = currentPet.sex.$t;
+						const currentPetSize = currentPet.size.$t;
 
-					var petElement = $('#model-pet').find('.pet').clone();
+						var petElement = $('#model-pet').find('.pet').clone();
+						petElement.attr('id', i);
 
+						petElement.find('img').attr('src', currentPetPic);
+						petElement.find('.pet-name').text(currentPetName);
+						petElement.find('.pet-breed').text(currentPetBreed);
+						petElement.find('.pet-age').text(currentPetAge + ' |');
+						petElement.find('.pet-gender').text(currentPetGender + ' | ');
+						petElement.find('.pet-size').text(currentPetSize);
 
-					petElement.find('img').attr('src', currentPetPic);
-					petElement.find('.pet-name').text(currentPetName);
-					petElement.find('.pet-breed').text(currentPetBreed);
-					petElement.find('.pet-age').text(currentPetAge + ' |');
-					petElement.find('.pet-gender').text(currentPetGender + ' | ');
-					petElement.find('.pet-size').text(currentPetSize);
-
-					$('#pet-choices').append(petElement);
-
+						$('#pet-choices').append(petElement);
+					}
 				}
+				$(".modal-launcher, #modal-background, #modal-close").click(function () {
+					$("#modal-content, #modal-background").toggleClass("active");
+					var petIndex = $(this).attr('id');
+					
 
-
-
-
-
-				
-				$('#pet1').find('img').attr('src', firstPetPic);
-				$('#pet1').find('.pet-name').text(firstPetName);
-				$("#pet1").find('.pet-breed').text(firstPetBreed);
-				$('#pet1').find('.pet-age').text(firstPetAge + ' |');
-				$('#pet1').find('.pet-gender').text(firstPetGender + ' | ');
-				$('#pet1').find('.pet-size').text(firstPetSize);
-
-				// const secondPet = petArray[1]
-				// const secondPetPic = secondPet.media.photos.photo[0].$t
-				// const secondPetName = secondPet.name.$t;
-				// const secondPetBreed = secondPet.breeds.breed[0].$t
-				// const secondPetAge = secondPet.age.$t;
-				// const secondPetGender = secondPet.sex.$t;
-				// const secondPetSize = secondPet.size.$t;
-
-
-				// $('#pet2').find('img').attr('src', secondPetPic);
-				// $('#pet2').find('.pet-name').text(secondPetName);
-				// $('#pet2').find('.pet-breed').text(secondPetBreed);
-				// $('#pet2').find('.pet-age').text(secondPetAge + ' |');
-				// $('#pet2').find('.pet-gender').text(secondPetGender + ' | ');
-				// $('#pet2').find('.pet-size').text(secondPetSize);
-				// console.log(secondPetGender);
-
+					if (petIndex !== 'modal-close') {
+						var currentPet = petArray[petIndex];
+						console.log(currentPet);
+						const currentPetName = currentPet.name.$t;
+						console.log(currentPetName);
+						$('#modal-content').find('.pet-name').text(currentPetName);
+					}
+				});	
 			},
 			error: function(request, error) {
 				console.log('error', request);
 			}
-		})
+		});
 	}
 });
+
+//Things to fix
+
+//1. Change animals to dropdown !!
+//2. The model (page 3) when you click on animals !!
+//3. Style modal
+//3. Making animals and names clickable
+//4. Having animals show up on map
+//5. Fix quality of images
+//6. Fix responsive design
+//7. Change M/F and S/M/L to Male/Female and small/med./large
+//8. Search by gender
+//9. Search by breed
+//10. Handle no results found
+//11. Get new search functioning
+
+
+
